4ecf4153ba0b4c0112317c15a639524a0f92ecb3
ruthearle <rrearle@gmail.com>
2014-08-06 13:31:42 +0100

    v6.Exercise 5 further student info

--- directory.rb
+++ directory.rb
@@ -2,26 +2,70 @@ student_count = 23
 # Lets put all students into array
 students = []
 
+# Exercise 6 - more student info
 def input_students
-	puts "Please enter the names of the students"
-	puts "To finish, just hit return twice"
+	puts "Please enter details of the students"
+	puts "To finish, just hit return 5 times"
 	# create an empty array
 	students = []
-	# get the full name
-	puts "Students full name?"
+	# get full name
+	puts "Full name"
 	name = gets.chomp
-	# while the name is not empty, repeat this code
+	# get hobbies
+	puts "Preferred hobby"
+	hobby = gets.chomp
+	# get country of birth
+	puts "Country of birth"
+	country = gets.chomp
+	# get students height in cm
+	puts "Height in cm"
+	height = gets.chomp
+	# get student weight
+	puts "Weight in kg"
+	weight = gets.chomp
+	# while the name is empty, repeat this code
 	until name.empty? do
-		#add the student hash to the array
-		students << {:name => name.capitalize, :cohort => :august.capitalize}
+		# add the student hash to the array
+		students << {:name => name.upcase, :hobby => hobby, :country => country.capitalize, :height => height, :weight => weight, :cohort =>:august.capitalize}
 		puts "Now we have #{students.length} students"
-		# get another name from the user
-		puts "Students full name?"
+		# get another student's details
+			puts "Full name"
 		name = gets.chomp
+		# get hobbies
+		puts "Preferred hobby"
+		hobby = gets.chomp
+		# get country of birth
+		puts "Country of birth"
+		country = gets.chomp
+		# get students height in cm
+		puts "Height in cm"
+		height = gets.chomp
+		# get student weight
+		puts "Weight in kg"
+		weight = gets.chomp
 	end
-	# return the array of students
 	students
 end
+
+#def input_students
+#	puts "Please enter the names of the students"	puts "To finish, just hit return twice"
+#	# create an empty array
+#	students = []
+#	# get the full name
+#	puts "Students full name?"
+#	name = gets.chomp
+#	# while the name is not empty, repeat this code
+#	until name.empty? do
+#		#add the student hash to the array
+#		students << {:name => name.upcase, :cohort => :august.capitalize}
+#		puts "Now we have #{students.length} students"
+#		# get another name from the user
+#		puts "Students full name?"
+#		name = gets.chomp
+#	end
+#	# return the array of students
+#	students
+#end
 
 
 def print_header
@@ -65,7 +109,7 @@ end
 def print(students)
 	counter = 0
 	while counter < students.size
-		puts "#{students[counter][:name]} (#{students[counter][:cohort]} cohort)"
+		puts "#{students[counter][:name]}: Hobby - #{students[counter][:hobby]}, Country of birth - #{students[counter][:country]}, Height - #{students[counter][:height]}, Weight - #{students[counter][:weight]}  (#{students[counter][:cohort]} cohort)"
 		counter += 1
 	end
 end
