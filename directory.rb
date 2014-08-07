a222f531aad6a5c86733eed63880024150bccfac
ruthearle <rrearle@gmail.com>
2014-08-07 18:01:39 +0100

    v.7 interactive menu added

--- directory.rb
+++ directory.rb
@@ -1,6 +1,28 @@
 student_count = 23
 # Lets put all students into array
-students = []
+
+def interactive_menu
+	students = []
+	loop do
+		# 1. print the menu and ask the user what to do
+		puts "1. Input the students"
+		puts "2. Show the students"
+		puts "9. Exit"
+		# 2. read the input and save it into a variable
+		selection = gets.chomp
+		# 3. do what the user has asked
+		case selection
+			when "1"
+				# input students
+			when "2"
+				# show students
+			when "9"
+				exit # this will cause the program to terminate
+			else
+				puts "I don't know what you meant, try again"
+			end
+	end
+end
 
 def input_students
 	puts "Please enter the names of the students and then enter the month the cohort started"	
@@ -83,7 +105,7 @@ def print_cohort(students)
 end
 
 def print_footer(names)
-	if students.length == 1
+	if names.length == 1
 		puts "Overall, we have #{names.length} great student".center(80, "***")
 	else
 		puts "Overall, we have #{names.length} great students".center(80, "***")
@@ -92,8 +114,9 @@ end
 
 #nothing happens until we call the methods
 
-students = input_students
+#students = input_students
+interactive_menu
 print_header
-#print(students)
+print(students)
 print_cohort(students)
 print_footer(students)
